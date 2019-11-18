dba111a518ec886d88b8f41049151dda767a9efb
Rico Mitchell <ricomitch@icloud.com>
2019-11-18 04:41:00 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,6 @@
-module Memorable 
-  module ClassMethods
+module Memorable
+module ClassMethods  
+  
   def reset_all
     self.all.clear
   end
@@ -7,9 +8,8 @@ module Memorable
   def count
     self.all.count
   end
-end
-
-module InstanceMethods
+  
+  module InstanceMethods
   def initialize
       self.class.all << self
     end
